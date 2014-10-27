2a3c5fde5e04d121987543613cb3abad9239cc17
Thomas Leister <leisterthomas@gmx.de>
2014-10-27 19:11:25 +0100

    Increase version number!

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * #################### This is WebMUM Version 0.1.2 ######################
+ * #################### This is WebMUM Version 0.1.3 ######################
  * 
  * Project on GitHub: https://github.com/ThomasLeister/webmum
  * Author's Blog: https://thomas-leister.de
