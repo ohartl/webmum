6cca28afca786228a26ff99a2dcd2a9de32c14ab
Thomas Leister <leisterthomas@gmx.de>
2014-12-22 17:16:19 +0100

    Version increase to 0.1.8

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * #################### This is WebMUM Version 0.1.7 ######################
+ * #################### This is WebMUM Version 0.1.8 ######################
  * 
  * Project on GitHub: https://github.com/ThomasLeister/webmum
  * Author's Blog: https://thomas-leister.de
