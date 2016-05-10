5c0fe41aba3075d73f429a9c7b0dc1c0da03f8ca
Leo Graf <leograf@posteo.de>
2016-05-10 03:32:25 +0200

    add meta elements for mobiles

--- include/php/template/layout.php
+++ include/php/template/layout.php
@@ -2,6 +2,11 @@
 <html>
 <head>
 	<title>WebMUM</title>
+	<meta http-equiv="cleartype" content="on">
+	<meta name="MobileOptimized" content="320">
+	<meta name="HandheldFriendly" content="True">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
 	<link rel=stylesheet href="<?php echo Router::url('include/css/webmum.style.css?v=1.00'); ?>" type="text/css" media=screen>
 	<script type="text/javascript">
 		function generatePassword() {
