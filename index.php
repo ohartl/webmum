3594c6e236453442c256bf87999ae21d7eac4a9e
Oliver Hartl <oliver@ohartl.de>
2016-09-08 21:50:15 +0200

    Running webmum with php built-in server

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
 
+if (php_sapi_name() == "cli-server") {
+	// running under built-in server
+	$extensions = array("php", "jpg", "jpeg", "css", "js");
+	$path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+	$ext = pathinfo($path, PATHINFO_EXTENSION);
+	if (in_array($ext, $extensions)) {
+		return false;
+	}
+}
+
 try {
 	/**
 	 * Loading system
